3ec7fa4156e71a2670ff0d54490296d21ade42a9
Bahar <131577601+Baharspring7@users.noreply.github.com>
2023-05-03 10:00:30 +0200

    Add files via upload

---
+++ BAHAR-1.js
@@ -0,0 +1,66 @@
+//Addition
+
+function bb(X){
+    for(int i=1; i<=200; i++)
+if (x % 3 == 0){
+    result: print
+}
+if (x % 5 == 0){
+    result: print
+    }
+console,log(bb)
+}
+
+
+
+
+//Shift the Values
+
+
+const oldArray= ['moslem','orhan','saeed','eva'];
+    console.log(oldArray);
+    const newArray = [...oldArray].reverse(oldArray);
+    console.log(oldArray);
+    console.log(newArray);
+
+
+//FizzBuzz
+fbnums <= 200 : 2700
+for (i in fbnums){
+    if (i %% 3 == 0 && i %% 5 ==0) {
+        output[i] <-"fizzbuzz"
+       }   else if (i %% 3 == 0){
+        output [i] <- "fizz"
+    }   else if (i %% 5 == 0){
+        output [i] <- "buzz"
+       }
+       else {
+        output[i] <-i
+       }
+    }
+    print(output)
+
+    //Fibonacci
+
+    let n = 15,i = 1, fib = [0,1];
+    while(++i <n)
+    fib[i] = fib[i-1]+fib[i-2]  
+
+
+       
+
+    //Remove the Negativ;
+
+let number = [-1,-2,0,3,4,-8 ];
+i = numbers.indexOf(-1,-2,0);
+if(i <= 0);{
+    numbers.splice(i,1);
+    console.log(number);
+}
+//or..
+let numbers = [-1,-2,0,3,4,-8];
+    numbers.splice[-1,-2,0];
+    console.log(numbers);
+    
+
+
